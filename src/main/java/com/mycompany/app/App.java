37dd3cba05f89c30fcb0f86fc9854bf0d0c0958d
mjabir <muhammad.jabir@yahoo.com>
2019-05-20 09:50:19 +0000

    webhook t2

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! webhook";
+    private final String message = "Hello World! webhook t2";
 
     public App() {}
 
